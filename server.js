a1f1c5a5767ab2df33eafad42cdbe07b5fb9c915
Karol Kropisz <karolkropisz@gmail.com>
2024-04-22 21:57:04 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
+const io = socket(server);
+
+let tasks =[];
+
+io.on('connection', (socket) => {
+
+  socket.emit('updateData', tasks)
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task)
+  });
+
+  socket.on('removeTask', (taskId) => {
+    tasks = tasks.filter(task => task.id !== taskId);
+    socket.broadcast.emit('removeTask', taskId);
+});
+
+
+});
+
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
